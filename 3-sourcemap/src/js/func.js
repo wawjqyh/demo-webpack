df65160b9c64f85d9fc77d0030b501c6087c0c9c
Troy <troy@meishakeji.com>
2020-05-26 14:09:38 +0800

    source map

---
+++ 3-sourcemap/src/js/func.js
@@ -0,0 +1,5 @@
+function hello() {
+  console.log('2333');
+}
+
+export default hello;
---
+++ 3-sourcemap/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+
+  entry: {
+    app: './src/index.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].min.[hash:5].js'
+  },
+
+  // 开启 source map
+  devtool: 'source-map',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'hello world',
+      filename: 'index.html', //打包后的文件名字
+      template: 'index.html', //源文件的名字
+      inject: 'head' // 页面引用文件的位置
+    }),
+
+    // 打包前删除 dist 目录
+    new CleanWebpackPlugin()
+  ]
+};
